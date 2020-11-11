93435077c1d5777706d77aa8a96068767092e642
Deidre-Ann <d_jemison@hotmail.com>
2020-11-10 23:57:58 -0500

    Attempted to implement Exercise 3 - Look up any Superhero by typing in the name or alias in a Textfield

--- app.js
+++ app.js
@@ -33,5 +33,30 @@ window.onload= function()
 			}
 		}
 	}
+
+
+	function searchResult(str) 
+	{
+  		if (str.length == 0) 
+  		{
+    		document.getElementById("supLst").innerHTML = "";
+    		return;
+  		} 
+  		else 
+  		{
+    		var xmlhttp = new XMLHttpRequest();
+    		xmlhttp.onreadystatechange = function() 
+    		{
+      			if (this.readyState == 4 && this.status == 200) 
+      			{
+        		document.getElementById("supLst").innerHTML = this.responseText;
+      			}
+    		}
+    	}
+    	xmlhttp.open("GET", "http://localhost/info2180-lab4/superheroes.php" + str, true);
+    	xmlhttp.send();		
+  	}
 };
+
+
 
